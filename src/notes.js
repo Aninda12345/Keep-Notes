ca1a21085b3593d5752a882c60580657b8729cca
Aninda12345 <anindasaha241@gmail.com>
2024-11-18 21:16:41 +0530

    update notes

--- src/notes.js
+++ src/notes.js
@@ -1,18 +1,18 @@
 const notes = [
   {
-    title: "Delegation",
+    title: "DBMS",
     content:
-      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem",
+      "DBMS stands for Database management system. A database management system (DBMS) is a software system for creating and managing databases. A DBMS enables end users to create, protect, read, update and delete data in a database.",
   },
   {
-    title: "Loops",
+    title: "Abstraction",
     content:
-      "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat.",
+      "Abstraction is a fundamental concept in object-oriented programming (OOPS) that involves hiding the internal details of an application from the outside world.",
   },
   {
     title: "Array vs Linked list",
     content:
-      "An array is a collection of data elements of the same type but A linked list is a collection of entities known as nodes. The node is divided into two sections: data and address.Array keeps the data elements in a single memory but linkedlist stores elements at random, or anywhere in the memory..",
+      "An array is a collection of data elements of the same type but A linked list is a collection of entities known as nodes. The node is divided into two sections: data and address.",
   },
 ];
 
